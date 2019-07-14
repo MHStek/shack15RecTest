9d0f807e44fd5c7994ca3cb67ad3da8a8bd0be65
Maclean Ayarik <ayarikmaclean@yahoo.com>
2019-07-14 12:46:56 +0000

    add method added

--- add.php
+++ add.php
@@ -2,8 +2,14 @@
 class ClassName
 {
     private $a;
+    
     public function _constract($a)
     {
         $this->a = $a;
+    }
+
+    public function add(float $a, float $b)
+    {
+        return $a + $b;
     }
 }
